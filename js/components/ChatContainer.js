8731916abf6fa092f31c175f3eb75b1ef5328e4d
dochinhthtn <dochinhthtn@gmail.com>
2021-03-22 20:56:25 +0700

    Session 9

--- js/components/ChatContainer.js
+++ js/components/ChatContainer.js
@@ -1,8 +1,9 @@
 const $template = document.createElement('template');
 $template.innerHTML = /*html*/ `
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <div id="chat-container">
         <message-list style="height: calc(100vh - 50px); display: block; overflow-y: scroll;"></message-list>
-        <send-message-form class="fixed-bottom pb-2"></send-message-form>
+        <send-message-form class="pb-2"></send-message-form>
     </div>
 `;
 
